d0710821a90048332de43d0a212481c664f2f48e
Jared Tulayan <2lyNJ.E@gmail.com>
2019-01-24 22:10:44 +0000

    Add body to LinkedList

--- LinkedLists/LinkedList.java
+++ LinkedLists/LinkedList.java
@@ -1,3 +1,19 @@
-public class LinkedList {
+public class LinkedList<A> {
+    private Node<A> first;
+
     
+
+    private class Node<A> {
+        private A value;
+        public Node<A> next;
+
+        public Node(A val, Node<A> n) {
+            value = val;
+            next = n;
+        }
+
+        public A getValue() {
+            return value;
+        }
+    }
 }
