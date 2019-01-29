2e74bfd7143690a2745bf655ccca9152c57e6d1a
Jared Tulayan <2lyNJ.E@gmail.com>
2019-01-29 22:51:39 +0000

    Fix removeValue method

--- LinkedLists/LinkedList.java
+++ LinkedLists/LinkedList.java
@@ -36,6 +36,26 @@ public class LinkedList<A> {
         // Display
         // Should be [4 5 6 7 2 3]
         System.out.println(list);
+
+        // Remove the 3 in the list
+        list.removeValue(3);
+        
+        // Remove the 2 in the list
+        list.remove(4);
+
+        // Display
+        // Should be [4 5 6 7]
+        System.out.println(list);
+
+        // Remove the first value in the list
+        list.remove(0);
+
+        // Remove the 5 in the list
+        list.removeValue(5);
+
+        // Display
+        // Should be [6 7]
+        System.out.println(list);
     }
 
     public A get(int index) {
@@ -91,6 +111,35 @@ public class LinkedList<A> {
     }
 
     /**
+     * Remove first instance of value
+     * 
+     * @param val the value to search for
+     * @return the value that was removed, or null if unsuccessful.
+     */
+    public A removeValue(A val) {
+        Node cur = front;
+        A removedVal = null;
+
+        if (front.getValue().equals(val)) {
+            removedVal = front.getValue();
+            front = front.getNext();
+        }
+
+        while (cur != null && cur.getNext() != null) {
+            Node next = cur.getNext();
+            if (next.getValue().equals(val)) {
+                removedVal = next.getValue();
+                cur.setNext(next.getNext());
+                break;
+            }
+
+            cur = next;
+        }
+
+        return removedVal;
+    }
+
+    /**
      * Gets the String representation of this LinkedList
      */
     public String toString() {
@@ -114,14 +163,14 @@ public class LinkedList<A> {
     /**
      * Searches for the first instance of the target value
      * 
-     * @param target the value to search for
+     * @param val the value to search for
      * @return the first Node containing the value being searched, or null if not found
      */
-    private Node search(A target) {
+    private Node search(A val) {
         Node cur = front;
 
         while (cur != null) {
-            if (cur.getValue().equals(target))
+            if (cur.getValue().equals(val))
                 break;
 
             cur = cur.getNext();
