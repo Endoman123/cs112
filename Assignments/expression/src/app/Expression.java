ab2fc913e1a485dd1dad9c62459ef9ffad6d7b61
Jared Tulayan <2lyNJ.E@gmail.com>
2019-02-17 15:16:41 +0000

    Begin shunting-yard implementation
    Can now do MDAS, need to implement paretheses

--- Assignments/expression/src/app/Expression.java
+++ Assignments/expression/src/app/Expression.java
@@ -11,7 +11,8 @@ public class Expression {
 	public static String delims = " \t*+-/()[]";
     private static final Pattern
         VAR_SPLIT = Pattern.compile("[\\d()+\\-/*]|[A-Za-z]+\\b\\[.+\\]"), 
-        ARR_PATTERN = Pattern.compile("[A-Za-z]+\\b(?=\\[.+\\])");
+        ARR_PATTERN = Pattern.compile("[A-Za-z]+\\b(?=\\[.+\\])"),
+        TOKEN_SPLIT = Pattern.compile("(?<=[\\-\\+\\*\\/\\(\\)\\[\\]])|(?=[\\-\\+\\*\\/\\(\\)\\[\\]])");
 
 
     /**
@@ -26,14 +27,16 @@ public class Expression {
      * @param arrays The arrays array list - already created by the caller
      */
     public static void makeVariableLists(String expr, ArrayList<Variable> vars, ArrayList<Array> arrays) {
-        for (String s : VAR_SPLIT.split(expr)) { // Find variable names
+        String exp = expr.replace(" ", "");
+
+        for (String s : VAR_SPLIT.split(exp)) { // Find variable names
             Variable temp = new Variable(s);
             
             if (!vars.contains(temp))
                 vars.add(temp);
         }
 
-        for (String s : ARR_PATTERN.split(expr)) { // Find array names
+        for (String s : ARR_PATTERN.split(exp)) { // Find array names
             Array temp = new Array(s);
             
             if (!arrays.contains(temp))
@@ -51,57 +54,82 @@ public class Expression {
      */
     public static float evaluate(String expr, ArrayList<Variable> vars, ArrayList<Array> arrays) {
         float ret = 0;
-        String temp = expr;
-        Stack<Integer> operands = new Stack<>();
+        Stack<Float> operands = new Stack<>();
 
         Stack<String>
             operators = new Stack<>(),
             brackets = new Stack<>();
 
         // Tokenize expresion
-        String[] tokens = temp.split("(?<=[\\-\\+\\*\\(\\)\\[\\]])|(?=[\\-\\+\\*\\(\\)\\[\\]])");
+        String[] tokens = TOKEN_SPLIT.split(expr.replace(" ", ""));
         System.out.println(Arrays.toString(tokens));
 
-        for (int i = tokens.length - 1; i > -1; i--) {
-            if (tokens[i].matches("\\d+")) { // If a number
-                operands.push(Integer.parseInt(tokens[i]));
-            } else if (tokens[i].matches("[A-Za-z]+")) { // If a variable
+        // Perform Shunting-yard algorithm
+        // Evaluate as we go
+        for (String token : tokens) {
+            if (token.matches("\\d+")) { // If a number
+                operands.push(Float.parseFloat(token));
+            } else if (token.matches("[A-Za-z]+")) { // If a variable
                 for (Variable v : vars) {
-                    System.out.println(v.name + " == " + tokens[i]);
-                    if (tokens[i].equals(v.name)) {
-                        System.out.println("Found " + v.name);
-                        operands.push(v.value);
+                    System.out.println(v.name + " == " + token);
+                    if (token.equals(v.name)) {
+                        System.out.println("found");
+                        operands.push((float) v.value);
                         break;
                     }
                 }
-            } else if (tokens[i].matches("[+\\-*/]")) { // If an operator
-                operators.push(tokens[i]);
+            } else if (token.matches("[+\\-*/]")) { // If an operator
+                switch(token) {
+                    case "*": // High-precedence operators
+                    case "/":
+                        operators.push(token);
+                        break;
+                    case "+": // Low-precedence operators
+                    case "-":
+                        if (!operators.isEmpty() && "*/".contains(operators.peek())) { // If there aren't any operators of precedence.
+                            while (!operators.isEmpty() || !"+-".contains(operators.peek())) { // Unti there is at most an operator of equal precedence
+                                float op2 = operands.pop(), op1 = operands.pop();
+                                
+                                operands.push(evaluate(operators.pop(), op1, op2));
+                            }
+                        }
+
+                        operators.push(token);
+                }
             }
         }
 
-        // Expression parse
+        // Final expression parse
+        while (!operators.isEmpty()) {
+            float op2 = operands.pop(), op1 = operands.pop();
+                                
+            operands.push(evaluate(operators.pop(), op1, op2));
+        }
+
         ret = operands.pop();
 
-        while(!operands.isEmpty()) {
-            System.out.println("Operand: " + operands.peek());
-            switch (operators.pop()) {
-                case "+": // Add
-                    System.out.println("Add");
-                    ret += operands.pop();
-                    break;
-                case "-": // Subtract
-                    System.out.println("Subtract");
-                    ret -= operands.pop();
-                    break;
-                case "*": // Multiply
-                    System.out.println("Multiply");
-                    ret *= operands.pop();
-                    break;
-                case "/": // Divide
-                    System.out.println("Divide");
-                    ret /= operands.pop();
-                    break;
-            }
+        return ret;
+    }
+
+    /**
+     * Helper function to evaluate simple expressions
+     */
+    private static float evaluate(String op, float op1, float op2) {
+        float ret = 0;
+
+        switch(op) {
+            case "*":
+                ret = op1 * op2;
+                break;
+            case "/":
+                ret = op1 / op2;
+                break;
+            case "+":
+                ret = op1 + op2;
+                break;
+            case "-":
+                ret = op1 - op2;
+                break;
         }
 
         return ret;
